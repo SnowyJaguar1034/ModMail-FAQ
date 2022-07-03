66786d50be745c443650fb7368b9050132bcdb8c
SnowyJaguar1034 <51423344+SnowyJaguar1034@users.noreply.github.com>
2022-07-03 22:53:45 +0100

    + private bots listing & base file for dataclasses

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import asyncio
-from logging import (
-    INFO,
-    WARNING,
-    FileHandler,
-    Formatter,
-    StreamHandler,
-    basicConfig,
-    getLogger,
-)
+from asyncio import run
+from logging import INFO, FileHandler, Formatter, StreamHandler, basicConfig, getLogger
+from typing import Optional
 
-from discord import Activity, ActivityType, Embed, Intents, Interaction
+from discord import Activity, ActivityType, Embed, Intents, Interaction, app_commands
 from discord.ext import commands
 from discord.ui import View
 
@@ -18,6 +11,7 @@ from classes.faq import FAQ_Client
 
 intents = Intents.default()
 intents.message_content = True
+intents.members = True
 
 client = FAQ_Client(
     intents=intents,
@@ -51,7 +45,7 @@ log = getLogger(__name__)
 data_loaded = None
 
 
-@client.tree.command()
+@client.tree.command(name="faq", description="Get support for ModMail")
 async def faq(interaction: Interaction):
     view = View()
     view.add_item(AlphaDropdown())
@@ -62,5 +56,56 @@ async def faq(interaction: Interaction):
     await interaction.response.send_message(embed=embed, view=view, ephemeral=True)
 
 
+@client.tree.command(name="post", description="Post the standalone help menu")
+@app_commands.describe(ephemeral="Accepts 'True' or 'False")
+async def post(interaction: Interaction, ephemeral: Optional[bool] = False):
+    view = View()
+    view.add_item(AlphaDropdown())
+    embed = Embed(
+        title="Welcome to the ModMail Help Center!",
+        description='This is an **interactive FAQ** where you can find answers to common questions about ModMail. Use the Select Menu below to navigate through the FAQ. You can go back to the previous topic by clicking the "Back" button',
+    )
+    await interaction.response.send_message(embed=embed, view=view, ephemeral=ephemeral)
+
+
+@client.tree.command(
+    name="private-instances",
+    description="Shows a list of all the private instances hosted by James",
+)
+async def private_instances(interaction: Interaction):
+    # bots is a dictioniry of bot ID to bot name and bot owner
+    bots = {}
+    ignored = [
+        859584658676383754,  # Go Postal
+        941314754851524639,  # Zupie
+        986757903090327552,  # FAQ
+        933684530026545193,  # Elf
+        799403868534079509,  # Alpha
+        575252669443211264,  # Modmail
+        692547870183915618,  # Syndicate
+        699033133550665788,  # Modmail Beta
+        785360543665487874,  # ModMail Alpha
+    ]
+    pages = []
+    hosted = client.get_guild(607652789304164362)
+    for member in hosted.members:
+        if member.bot:
+            if member.id not in ignored:
+                bots[member.id] = member.name
+
+    embed = Embed(title="Private Instances")
+    embed2 = Embed(title="Private Instances")
+    for bot in bots:
+        if len(embed.fields) <= 24:
+            embed.add_field(name=bot, value=bots[bot])
+        elif len(embed.fields) > 24:
+            embed2.add_field(name=bots[bot], value=bot)
+
+    pages.append(embed)
+    pages.append(embed2)
+
+    await interaction.response.send_message(embeds=pages, ephemeral=True)
+
+
 if __name__ == "__main__":
-    asyncio.run(client.main())
+    run(client.main())
