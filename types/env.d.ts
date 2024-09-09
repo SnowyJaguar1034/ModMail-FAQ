d8e69ab7f1b73486e6ff1c71b7947b59ef2ba9ed
SnowyJaguar1034 <github@snowyjaguar.xyz>
2024-09-09 19:41:36 +0100

    remove redundant ts/js files

--- types/env.d.ts
+++
@@ -1,6 +0,0 @@
-interface Env {
-  // Add KV namespaces, Durable Object bindings, secrets, etc. here
-  // KV_NAMESPACE: KVNamespace;
-  // DURABLE_OBJECT: DurableObjectNamespace;
-  // SECRET: string;
-}
